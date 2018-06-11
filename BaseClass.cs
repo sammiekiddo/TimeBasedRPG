d05a8a890ec966c7cc838073e58c854362d09d90
sammiekiddo <samanthahowze@gmail.com>
2018-06-11 16:23:12 -0500

    Base script for time based RPG type game

---
+++ BaseClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;//for List
+using UnityEngine;
+
+
+
+
+[System.Serializable]
+public class BaseClass
+{
+	//public string name;
+	public string theName;
+
+	//changed from float
+	public int baseHP;
+	public float curHP;
+
+	public int baseMP;
+	public float curMP;
+
+	public int baseATK;
+	public float curATK;
+	public int baseDEF;
+	public float curDEF;
+
+	public List<BaseAttack> Attacks = new List<BaseAttack> ();//list for inputing melee attacks
+	//public List<BaseAttack> MagicAttack = new List<BaseAttack>();//list for inputing magic attacks
+
+
+}
